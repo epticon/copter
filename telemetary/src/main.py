bd262e742b42053f757836f7295b06c7640d9c4e
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2019-02-13 19:10:11 +0100

    Feat(Env): Pick Enviroment Variables From .env File

--- telemetary/src/main.py
+++ telemetary/src/main.py
@@ -1,6 +1,13 @@
 import asyncio
+import config.settings
 from telemetary import Telemetary
+from dotenv import load_dotenv
+from dronekit import connect
+import os
+
+BROADCAST_ADDRESSES = os.getenv("TELEMETARY_BROADCAST_SERVERS", "").split(",")
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(Telemetary())
+    telemetary = Telemetary(BROADCAST_ADDRESSES)
+    loop.run_until_complete(telemetary.start())
